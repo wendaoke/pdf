fa51ae7291a4da1041d122a1fb3d49fc6864ba60
wendaoke <wendaoke@126.com>
2026-03-29 22:04:28 +0800

    百度统计代码

--- frontend/pdf-merge-web/src/app/layout.tsx
+++ frontend/pdf-merge-web/src/app/layout.tsx
@@ -1,10 +1,28 @@
+import Script from "next/script";
 import "./globals.css";
 import { Providers } from "./providers";
+
+/** 百度统计 hm.js 站点 id（来自 hm.baidu.com 后台） */
+const BAIDU_HM_ID = "8f86357874a0bccb4370dbc0c402b22a";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-CN">
       <body>
+        <Script
+          id="baidu-hm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?${BAIDU_HM_ID}";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();`
+          }}
+        />
         <Providers>{children}</Providers>
       </body>
     </html>
